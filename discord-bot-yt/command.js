cb400f6adbdd207ce6490ef412e70a341ffa2348
LavVadnagara1284 <lav.vadnagara@adit.com>
2025-02-26 18:10:18 +0530

    temp

--- discord-bot-yt/command.js
+++ discord-bot-yt/command.js
@@ -1,5 +1,9 @@
 const { REST, Routes } = require('discord.js');
 
+// BOT_TOKEN = MTM0NDI1Mjk1NTQ0Mzk4NjUyMw.GB21de.qaBkJJ0ahOflopQGjr5nZkXiUJC1GUzuCJNHgY
+
+// CLIENT_ID = 1344252955443986523
+
 const commands = [
     {
         name: 'ping',
