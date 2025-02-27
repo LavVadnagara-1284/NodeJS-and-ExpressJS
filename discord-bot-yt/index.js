a24b089abb1945f1be84812e1412a27505fb0dcc
LavVadnagara1284 <lav.vadnagara@adit.com>
2025-02-27 17:05:59 +0530

    6

--- discord-bot-yt/index.js
+++ discord-bot-yt/index.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 
+// BOT_TOKEN = MTM0NDI1Mjk1NTQ0Mzk4NjUyMw.GB21de.qaBkJJ0ahOflopQGjr5nZkXiUJC1GUzuCJNHgY
+
+// CLIENT_ID = 1344252955443986523
+
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -16,5 +20,10 @@ client.on('messageCreate', (message) => {
     })
 })
 
-client.login(process.env.BOT_TOKEN);
+client.on('interactionCreate', (interaction) => {
+    console.log(interaction);
+    interaction.reply('pong!')
+})
+
+client.login('MTM0NDI1Mjk1NTQ0Mzk4NjUyMw.GB21de.qaBkJJ0ahOflopQGjr5nZkXiUJC1GUzuCJNHgY');
 
